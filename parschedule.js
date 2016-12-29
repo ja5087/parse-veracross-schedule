7ecbffe5e3a56b0f30ffede7879b98781ded224f
Ja Wattanawong <pleasedontsendmailtome@gmail.com>
2016-12-29 22:50:12 +0700

    fixed to push all days
    reassigns blocks each time

--- parschedule.js
+++ parschedule.js
@@ -2,7 +2,7 @@
 /*
 Schedule
 -Days
---schoolClasses
+--SchoolBlocks
 */
 function Schedule(studentName, yearLevel) {
     this.studentName = studentName;
@@ -18,14 +18,14 @@ function Schedule(studentName, yearLevel) {
 }
 
 function Day(name) {
-    this.schoolClasses = [];
-    this.pushschoolClasses = function (_class) {
-        this.schoolClasses.push(_class);
+    this.SchoolBlocks = [];
+    this.pushSchoolBlocks = function (_class) {
+        this.SchoolBlocks.push(_class);
     }
     this.dayName = name;
 }
 
-function schoolClasses(name, startTime, endTime, teacherName, periodName, classroom) {
+function SchoolBlocks(name, startTime, endTime, teacherName, periodName, classroom) {
     this.name = name;
     this.startTime = startTime;
     this.endTime = endTime;
@@ -34,8 +34,27 @@ function schoolClasses(name, startTime, endTime, teacherName, periodName, classr
     this.classroom = classroom;
 }
 
+var mySchedule;
+
 var textTools = (function () {
     var pub = {};
+    pub.convertNameToObject = function(string) {
+        var nameArray = string.match(/[A-Z][a-z]+/g); //match all words
+        var name = {
+            prefName : "",
+            firstName : "",
+            lastName : ""
+        };
+        nameArray.push(nameArray.splice(0,1)[0]); //convert to right order.
+
+
+        //TODO: Check for middle name.
+        //prefName is shorter than firstName
+        name.prefName = (nameArray[0].length<nameArray[1].length) ? nameArray[0] : nameArray[1];
+        name.firstName = (nameArray[1].length<nameArray[0].length) ? nameArray[0] : nameArray[1];
+        name.lastName = nameArray[2];
+        return name;
+    }
     pub.parseData = function (strArray) {
 
         //data need to parse:
@@ -52,12 +71,19 @@ var textTools = (function () {
         //parse name
         data.name = strArray[0];
 
+        //test if there is time in data block 1 (for some)
+        timeInIndex0 = strArray[0].search(/-\s[0-9]+:[0-9]+/g);
+        if(timeInIndex0>0)
+        {
+            data.name = strArray[0].replace(/-\s[0-9]+:[0-9]+/g,"").trim();
+        }
 
-        console.log(strArray);
+        //console.log(strArray);
         //parse the times
 
 
         //quicky hacky to get Break 2 working since it stores the time in the first element instead
+        //time is either in i=1, or i=2.
         var timeIndex;
         try {
             if (typeof strArray[1] !== 'undefined')
@@ -83,11 +109,9 @@ var textTools = (function () {
         //parse the periodName if possible, otherwise write unknown again love the quicky hacky
         try {
             if (typeof strArray[1] !== 'undefined')
-                data.periodName = strArray[1].match(/(?:\(Period\s)((.+))(?:\))/)[1];
-            else
-                data.periodName = "unknown"
+                data.periodName = strArray[1].match(/(?:\(Period\s)((.+))(?:\))/)[1];                
         } catch (error) {
-            //ignore as usual
+            data.periodName = "none";
         }
 
 
@@ -96,15 +120,13 @@ var textTools = (function () {
             if (typeof strArray[2] !== 'undefined') {
                 data.teacherName = strArray[2].split("-")[0].trim();
                 data.classroom = strArray[2].split("-")[1].trim();
-            } else {
-                data.teacherName = "";
-                data.classroom = "";
             }
         } catch (error) {
-
+            data.teacherName = "none";
+            data.classroom = "none";
         }
 
-        console.log(data);
+        //console.log(data);
         return data;
     }
     return pub;
@@ -127,11 +149,11 @@ function parse() {
 
     //build  mapping between day_id and Letter Date
     var labels = doc.getElementById("labels");
-    console.log(labels.children);
+    //console.log(labels.childElementCount);
     var dayIDLists = {};
     for (var i = 0; i < labels.childElementCount; i++) {
         dayIDLists[labels.children[i].className] = labels.children[i].innerHTML;
-        console.log("updated dayidlists with: " + labels.children[i].className + " to " + labels.children[i].innerHTML);
+        //console.log("updated dayidlists with: " + labels.children[i].className + " to " + labels.children[i].innerHTML);
     }
 
 
@@ -144,17 +166,29 @@ function parse() {
     var studentName, studentYearLevel;
     studentName = doc.getElementById("header").children[0].innerHTML.trim();
     studentYearLevel = doc.getElementById("paragraphs").children[2].innerHTML.match(/[0-9]+/)[0];
-    var mySchedule = new Schedule(studentName, studentYearLevel);
+    
+    
+    mySchedule = new Schedule(textTools.convertNameToObject(studentName), studentYearLevel);
 
 
 
     //iterate three levels
     for (var x = 0; x < scheduleDays.length; x++) { //pushes every day of the schedule
-        var newDay = new Day(dayIDLists[scheduleDays[0].className.split(" ")[1]]); //get the proper day name by splitting the className from format "day day_20" to "day_20"
+        var newDay = new Day(dayIDLists[scheduleDays[x].className.split(" ")[1]]); //get the proper day name by splitting the className from format "day day_20" to "day_20"
+        scheduleBlocks = scheduleDays[x].getElementsByClassName("block");
         for (var i = 0; i < scheduleBlocks.length; i++) {  //pushes every class of the day
             var stringArray = scheduleBlocks[i].children[0].innerHTML.split(/\<br\>/g);
             var data = textTools.parseData(stringArray);
-            var newClass = new schoolClasses(
+
+            /*//check that all values are defined
+            for(key in data)
+            {
+                assert(typeof data.key !== 'undefined', "Undefined value detected in data." + key);
+            }
+             */
+
+
+            var newClass = new SchoolBlocks(
                 data.name,
                 data.startTime,
                 data.endTime,
@@ -162,7 +196,7 @@ function parse() {
                 data.periodName,
                 data.classroom
             );
-            newDay.pushschoolClasses(newClass);
+            newDay.pushSchoolBlocks(newClass);
         }
 
         mySchedule.pushDays(newDay);
@@ -173,6 +207,7 @@ function parse() {
     node.innerHTML = JSON.stringify(mySchedule);
     var e = document.getElementById("itembox");
     e.appendChild(node);
+    console.log(mySchedule);
 }
 
 
